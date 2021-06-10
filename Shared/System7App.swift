2cd623af83c90a8388ff0e882a753017d6b8601b
Cameron Perry <cameron.perry@valtech.com>
2021-06-09 20:02:32 -0700

    Initial Commit

---
+++ Shared/System7App.swift
@@ -0,0 +1,17 @@
+//
+//  System7App.swift
+//  Shared
+//
+//  Created by Cameron Perry on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct System7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
